d15ee9653eddc5d37c67fdbffc167a730656b639
C4illin <emrik.ostling@gmail.com>
2021-06-23 13:05:32 +0200

    added google maps

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const colors = randomColor({count: sectLength-sectColor.length})
 
 sectColor = sectColor.concat(colors)
 
-sectColor[5] = 'black'
-sectColor[6] = "#f46607"
+sectColor[6] = 'black'
+sectColor[7] = "#f46607"
 
 new fullpage('#fullpage', {
   sectionsColor: sectColor,
